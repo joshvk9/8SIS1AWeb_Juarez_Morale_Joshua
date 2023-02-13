c8965a540143a4293150655c6f175f2e813e710e
8SIS1AWeb_Juarez_Morale_Joshua <j.juarez.m3@edu.utc.mx>
2023-02-13 02:27:31 -0600

    Actividad

--- 05EjercicioJS/Ejercicio 2/JS/Ejercicio 2.js
+++ 05EjercicioJS/Ejercicio 2/JS/Ejercicio 2.js
@@ -1,62 +1,18 @@
-
-function validar(formulario){
-    if(formulario.nombre.value.length < 3){
-        alert("Escriba por lo menos 3 caracteres en el campo nombre");
-        formulario.nombre.focus();
-    }
-    var checkok ="QWERTYUIOPASDFGHJKLÑZXCVBNM" + 
-    "qwertyuiopasdfghjklñzxcvbnm";
-    var checkString = formulario.nombre.value;
-    var todoesvalido = true;
-    for(var i = 0; i< checkString.length;i++){
-        var ch = checkString.charAT(i);
-        for(var j =0; j< checkok.length; j++){
-            if(ch == checkok.charAt(j)){
-                break;
-            }
-        }
-        if(j == checkok.length){
-            todoesvalido = false;
-            break;
-        }
-
-    }
-    if(!todoesvalido){
-        alert("Escriba unicamente ltrass en el campo nombre");
-        formulario.nombre.focus();
-        return false;
-    }
-    var checkok ="1234567890";
-    var checkString = formulario.edad.value;
-    var todoesvalido = true;
-    for(var i = 0; i< checkString.length;i++){
-        var ch = checkString.charAT(i);
-        for(var j =0; j< checkok.length; j++){
-            if(ch == checkok.charAt(j)){
-                break;
-            }
-        }
-        if(j == checkok.length){
-            todoesvalido = false;
-            break;
-        }
-
-    }
-    if(!todoesvalido){
-        alert("Escriba unicamente ltrass en el campo edad");
-        formulario.edad.focus();
-        return false;
-    }
-
-    var txt = formulario.correo.value;
-
-    var b = /^[^@\s]+[^@\.\]+(\.[^@\.\s]+)+$/;
-
-    alert("Email" + (b.test(txt)? " " : " no")+ "valio");
-
-    return b.test;
+function calcular(){
+    var monto_ini =0;
+    var desc =0;
+    var imp_desc = 0;
+    var total =0;
     
+    monto_ini = document.getElementById('monto_inicial').value;
+    desc = document.getElementById('descuento').value;
+    
+
+    imp_desc = (monto_ini * desc)/100;
+
+    document.getElementById('importe_descontado').value=imp_desc;
+
+    total = monto_ini - imp_desc
+    document.getElementById('total').value=total;
+    console.log(total);
 }
-
-
-
